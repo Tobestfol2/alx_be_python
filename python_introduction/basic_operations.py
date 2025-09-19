d162f35d6d0ae8a4a4778f70b8bfb646db2f6bd2
Tobestfol2 <tobestfol2@gmail.com>
2025-09-19 17:33:10 +0100

    python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+                                       
+number2 = 5
+                                       
+add_result = (number1 + number2)
+                                       
+sub_result = (number1 - number2)
+                                       
+mult_result = (number1 * number2)
+                                       
+print("Addition of 10 and 5 is", int(add_result))
+print("Subtraction of 10 and 5 is", int(sub_result))
+print("Multiplication of 10 and 5 is", int(mult_result))
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+seconds = 60
+minutes = 2 * int(seconds) * 60
+print("2 hour(s) is", int(minutes))
